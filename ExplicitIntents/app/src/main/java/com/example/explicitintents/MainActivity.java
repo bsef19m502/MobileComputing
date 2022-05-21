f1c6f3424e2e0878ebe8098846c8433a9d33181d
Azka Noreen <azka.noreen6@gmail.com>
2022-05-21 12:04:22 +0500

    With Passing Values to other activity

--- ExplicitIntents/app/src/main/java/com/example/explicitintents/MainActivity.java
+++ ExplicitIntents/app/src/main/java/com/example/explicitintents/MainActivity.java
@@ -6,19 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button s;
+    TextView n;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         s=findViewById(R.id.sec);
+        n=findViewById(R.id.name);
         s.setOnClickListener(new View.OnClickListener() {
                                  @Override
         public void onClick(View view) {
                                      Intent in=new Intent(MainActivity.this,MainActivity2.class);
+                                     String name=n.getText().toString();
+                                     in.putExtra("name",name);
                                      startActivity(in);
 
 
--- ExplicitIntents/app/src/main/java/com/example/explicitintents/MainActivity2.java
+++ ExplicitIntents/app/src/main/java/com/example/explicitintents/MainActivity2.java
@@ -2,13 +2,20 @@ package com.example.explicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    TextView p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        p=findViewById(R.id.props);
+        Intent in=getIntent();
+        p.setText(in.getStringExtra("name"));
+
+
     }
 }
